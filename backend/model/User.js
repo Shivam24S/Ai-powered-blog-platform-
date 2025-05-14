4bb71fe231c60500d1fb54f2c31d766d32c2882d
Shivam24S <shivamshrivastav8055@gmail.com>
2025-05-14 20:08:07 +0530

    UserSchema created

---
+++ backend/model/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate(value) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(value)) {
+        throw new Error("invalid email format");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.toLowercase().includes("password")) {
+        throw new Error("Password cannot contain 'password' word as password");
+      }
+    },
+  },
+  profilePic: {
+    type: Buffer,
+  },
+  Blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+  ],
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
