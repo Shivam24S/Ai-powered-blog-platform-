5fca8187835bfe26a422810e675a7aeef986f67c
Shivam24S <shivamshrivastav8055@gmail.com>
2025-05-22 12:30:40 +0530

    staring routes added

---
+++ frontend/src/routes/MainNavigation.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const MainNavigation = () => {
+  return (
+    <>
+      <div>MainNavigation</div>
+      <Outlet />
+    </>
+  );
+};
+
+export default MainNavigation;
